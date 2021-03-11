fe516beced7764e7d9eed984b4112ab30c260d90
ElsaKing <69370516+ElsaKing@users.noreply.github.com>
2021-03-11 03:16:42 -0600

    Update jd_plantBean.js

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -47,7 +47,7 @@ let currentRoundId = null;//本期活动id
 let lastRoundId = null;//上期id
 let roundList = [];
 let awardState = '';//上期活动的京豆是否收取
-let randomCount = $.isNode() ? 20 : 5;
+let randomCount = $.isNode() ? 0 : 0;
 !(async () => {
   await requireConfig();
   if (!cookiesArr[0]) {

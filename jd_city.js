9a3e74fd91f1465163d476dc7aa7bc824eee5eab
ElsaKing <69370516+ElsaKing@users.noreply.github.com>
2021-05-25 00:58:28 -0500

    Update jd_city.js

--- jd_city.js
+++ jd_city.js
@@ -254,7 +254,7 @@ function city_lotteryAward() {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
-    $.get({url: `http://share.turinglabs.net/api/v3/city/query/10/`, 'timeout': 10000}, (err, resp, data) => {
+    $.get({url: `http://share.turinglabs.net/api/v3/city/query/0/`, 'timeout': 10000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)

f0715b9e98b1ea962d7958244a7e69b432504aff
ElsaKing <69370516+ElsaKing@users.noreply.github.com>
2021-06-03 01:55:37 -0500

    Update jd_gold_creator.js

--- jd_gold_creator.js
+++ jd_gold_creator.js
@@ -31,7 +31,6 @@ if ($.isNode()) {
     cookiesArr.push(jdCookieNode[item])
   })
   if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {};
-  if (JSON.stringify(process.env).indexOf('GITHUB') > -1) process.exit(0)
 } else {
   cookiesArr = [
     $.getdata("CookieJD"),
